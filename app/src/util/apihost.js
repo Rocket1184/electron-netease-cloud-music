56bc5e0dfbf79d277d10c4f533d9ca8adcdb2291
rocka <i@rocka.me>
2017-04-03 18:48:58 +0800

    got some fake api

--- app/src/util/apihost.js
+++ app/src/util/apihost.js
@@ -20,59 +20,18 @@ async function getMusicRecord(uid) {
     });
 }
 
-function getDailySuggestions() {
-    return [
-        {
-            id: 28243624,
-            name: 'ありふれたせかいせいふく',
-            artistName: 'ピノキオP'
-        },
-        {
-            id: 27950383,
-            name: 'FREELY TOMORROW',
-            artistName: 'Mitchie M'
-        },
-        {
-            id: 26219539,
-            name: 'こちら、幸福安心委员会です.',
-            artistName: 'うたたP'
-        },
-        {
-            id: 22699101,
-            name: '里表ラバーズ (reverse Edge mix by キャプテンミライ) - remix',
-            artistName: 'wowaka'
-        },
-        {
-            id: 29747141,
-            name: 'End of the World',
-            artistName: 'papiyon'
-        },
-        {
-            id: 29719542,
-            name: '文字恋',
-            artistName: 'GUMI'
-        },
-        {
-            id: 29017142,
-            name: '文学者の恋文',
-            artistName: 'doriko'
-        },
-        {
-            id: 28936917,
-            name: '精神崩壊シンドローム',
-            artistName: '初音ミク'
-        },
-        {
-            id: 28382729,
-            name: '1/6 -out of the gravity-',
-            artistName: 'ぼーかりおどP'
-        },
-        {
-            id: 28063259,
-            name: 'WAVE',
-            artistName: '鏡音リン'
-        }
-    ];
+async function getDailySuggestions() {
+    return await new Promise(resolve => {
+        setTimeout(() => {
+            resolve([
+                {
+                    id: 28243624,
+                    name: 'ありふれたせかいせいふく',
+                    artistName: 'ピノキオP'
+                }
+            ]);
+        }, 500);
+    });
 }
 
 export default {
