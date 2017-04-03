778da092a1ea577f5885c127ca7a6dcf4bfa1319
rocka <i@rocka.me>
2017-04-04 02:03:23 +0800

    api for userInfo(userPlaylist), dailySuggestions

--- app/src/util/apihost.js
+++ app/src/util/apihost.js
@@ -3,13 +3,11 @@ import crypto from 'crypto';
 import Client from './httpclient';
 
 const client = new Client();
-client.setCookie({
-    os: 'pc',
-    osver: 'Microsoft-Windows-10-Professional-build-10586-64bit',
-    appver: '2.0.3.131777',
-    channel: 'netease',
-    __remember_me: 'true'
-});
+
+function updateCookie(cookie) {
+    client.updateCookie(cookie);
+    return client.getCookie('');
+}
 
 function getCookie(key = '') {
     return client.getCookie(key);
@@ -39,6 +37,18 @@ async function login(acc, pwd) {
     }
 }
 
+async function getUserInfo(uid) {
+    return await client.post({
+        url: 'http://music.163.com/weapi/user/playlist',
+        data: {
+            uid,
+            offset: 0,
+            limit: 1,
+            csrf_token: ''
+        }
+    });
+}
+
 async function getMusicRecord(uid) {
     return await client.post({
         url: 'http://music.163.com/weapi/v1/play/record?csrf_token=',
@@ -51,22 +61,22 @@ async function getMusicRecord(uid) {
 }
 
 async function getDailySuggestions() {
-    return await new Promise(resolve => {
-        setTimeout(() => {
-            resolve([
-                {
-                    id: 28243624,
-                    name: 'ありふれたせかいせいふく',
-                    artistName: 'ピノキオP'
-                }
-            ]);
-        }, 500);
+    return await client.post({
+        url: 'http://music.163.com/weapi/v1/discovery/recommend/songs',
+        data: {
+            offset: 0,
+            total: true,
+            limit: 20,
+            csrf_token: ''
+        }
     });
 }
 
 export default {
     getCookie,
+    updateCookie,
     login,
+    getUserInfo,
     getMusicRecord,
     getDailySuggestions
 };
