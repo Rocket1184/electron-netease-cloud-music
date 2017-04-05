b82b96f9882ed1778c98ed8dda682b95804b9f86
rocka <i@rocka.me>
2017-04-05 17:28:43 +0800

    change background path

--- app/src/renderer/components/appnav.vue
+++ app/src/renderer/components/appnav.vue
@@ -166,7 +166,7 @@ export default {
         width: 100%;
         height: 200px;
         background-size: cover;
-        background-image: url(../../assets/TealRedYellow.png);
+        background-image: url(../../../assets/TealRedYellow.png);
         background-position-y: 50%;
         &::before {
             position: absolute;
