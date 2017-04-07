3a8dae47897a1b15b67956d2bbfd44f1aee8cda2
rocka <i@rocka.me>
2017-04-08 00:55:54 +0800

    test upload qiniu with travis

---
+++ script/upload-build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cd dist
+wget http://devtools.qiniu.com/qshell-v2.0.5.zip -O qshell.zip
+unzip qshell.zip qshell_linux_amd64
+./qshell_linux_amd64 account $QINIU_AK $QINIU_SK
+
+export APP_NAME=electron-netease-cloud-music
+export BUCKET_NAME=$APP_NAME-dev
+
+for ARCH in linux-x64 darwin-x64
+do
+    export TAR_NAME=$ARCH.tar.gz
+    tar zcf $TAR_NAME $APP_NAME-$ARCH
+    ./qshell_linux_amd64 rput $BUCKET_NAME $TAR_NAME $TAR_NAME
+done
