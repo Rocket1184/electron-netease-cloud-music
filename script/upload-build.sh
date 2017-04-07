8b0dd2d145762998d9d81f06cbc0f24edbd09ad0
rocka <i@rocka.me>
2017-04-08 01:18:15 +0800

    test qiniu

--- script/upload-build.sh
+++ script/upload-build.sh
@@ -4,6 +4,7 @@ cd dist
 wget http://devtools.qiniu.com/qshell-v2.0.5.zip -O qshell.zip
 unzip qshell.zip qshell_linux_amd64
 ./qshell_linux_amd64 account $QINIU_AK $QINIU_SK
+./qshell_linux_amd64 account
 
 export APP_NAME=electron-netease-cloud-music
 export BUCKET_NAME=$APP_NAME-dev
