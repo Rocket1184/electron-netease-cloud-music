5c7bc17b5cf6c59065adbd13b4091a1839e231ce
rocka <i@rocka.me>
2017-04-09 21:11:10 +0800

    getters for vuex module

--- app/src/renderer/vuex/getters.js
+++ app/src/renderer/vuex/getters.js
@@ -1,1 +1,29 @@
 export const mainCounter = state => state.counters.main;
+
+export const userAvatarUrl = state => {
+    return state.user.loginValid
+        ? state.user.profile.avatarUrl
+        : null;
+};
+
+export const userName = state => {
+    return state.user.loginValid
+        ? state.user.profile.nickname
+        : '未登录';
+};
+
+export const userBkgUrl = state => {
+    return state.user.loginValid
+        ? state.user.profile.backgroundUrl
+        : null;
+};
+
+export const playingMusic = state => {
+    return {
+        ...state.playing,
+        picUrl: state.playing.album.picUrl,
+        artist: Array.isArray(state.playing.artists)
+            ? state.playing.artists.reduce((i, j) => `${i} / ${j ? '' : j.name}`, '')
+            : state.playing.artists[0].name
+    };
+};
