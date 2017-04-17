23b6a1fd033fc29f54173c6b558cfea9441e7d85
rocka <i@rocka.me>
2017-04-17 11:14:56 +0800

    new current play list compo

---
+++ app/src/renderer/components/currentlist.vue
@@ -0,0 +1,41 @@
+<template>
+    <mu-list>
+        <template v-for="(song, index) in playlist.list">
+            <mu-list-item :title="song.name"
+                          :afterText="song.artists.map(i => i.name).join(' / ')"
+                          @click="handleListClick(index)">
+                <mu-icon v-if=""
+                         slot="left"
+                         :value="song.id==playingMusic.id ? 'volume_up' : ''" />
+            </mu-list-item>
+        </template>
+    </mu-list>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex';
+
+export default {
+    data() {
+        return {};
+    },
+    computed: {
+        ...mapGetters([
+            'playlist',
+            'playingMusic'
+        ])
+    },
+    methods: {
+        ...mapActions([
+            'playTrackIndex'
+        ]),
+        handleListClick(index) {
+            this.playTrackIndex({ index });
+        }
+    }
+};
+</script>
+
+<style lang="less">
+
+</style>
