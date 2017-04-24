17d4889cede32f576701328e8944ba5951dae3c5
rocka <i@rocka.me>
2017-04-24 17:36:24 +0800

    show a loading indicator when nothing shown

--- app/src/renderer/components/tracklist.vue
+++ app/src/renderer/components/tracklist.vue
@@ -1,27 +1,34 @@
 <template>
-    <div>
-        <mu-list-item title="播放全部"
-                      @click="handlePlayAll">
-            <mu-icon slot="left"
-                     value="play_circle_filled" />
-        </mu-list-item>
-        <mu-divider />
-        <mu-table class="playlist">
-            <template v-for="track in list">
-                <mu-tr>
-                    <mu-td :title="track.name">
-                        <span class="label">{{track.name}}</span>
-                    </mu-td>
-                    <mu-td :title="track.artistName">
-                        <span class="label">{{track | formatArtists}}</span>
-                    </mu-td>
-                    <mu-td>
-                        <mu-icon-button icon="favorite_border" />
-                        <mu-icon-button icon="playlist_add" />
-                    </mu-td>
-                </mu-tr>
-            </template>
-        </mu-table>
+    <div class="tracklist">
+        <template v-if="list.length !== 0">
+            <mu-list-item title="播放全部"
+                          @click="handlePlayAll">
+                <mu-icon slot="left"
+                         value="play_circle_filled" />
+            </mu-list-item>
+            <mu-divider />
+            <mu-table class="table">
+                <template v-for="track in list">
+                    <mu-tr>
+                        <mu-td :title="track.name">
+                            <span class="label">{{track.name}}</span>
+                        </mu-td>
+                        <mu-td :title="track.artistName">
+                            <span class="label">{{track | formatArtists}}</span>
+                        </mu-td>
+                        <mu-td>
+                            <mu-icon-button icon="favorite_border" />
+                            <mu-icon-button icon="playlist_add" />
+                        </mu-td>
+                    </mu-tr>
+                </template>
+            </mu-table>
+        </template>
+        <div v-else
+             class="loading-wrapper">
+            <mu-circular-progress :size="60"
+                                  :strokeWidth="5" />
+        </div>
     </div>
 </template>
 
@@ -53,14 +60,24 @@ export default {
 </script>
 
 <style lang="less">
-.playlist {
-    .label {
-        display: block;
-        overflow: hidden;
-        text-overflow: ellipsis;
+.tracklist {
+    width: 100%;
+    .table {
+        .label {
+            display: block;
+            overflow: hidden;
+            text-overflow: ellipsis;
+        }
+        td:nth-child(3) {
+            width: 140px;
+        }
     }
-    td:nth-child(3) {
-        width: 140px;
+    .loading-wrapper {
+        height: 400px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
     }
 }
 </style>
