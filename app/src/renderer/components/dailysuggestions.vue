073cbb459346be8e3f4e5ed4f6e348be0d434ef9
rocka <i@rocka.me>
2017-04-24 11:21:51 +0800

    adjust daily suggestions styles

--- app/src/renderer/components/dailysuggestions.vue
+++ app/src/renderer/components/dailysuggestions.vue
@@ -2,7 +2,8 @@
     <mu-paper class="daily-suggestions">
         <div class="cell title">
             <p>{{dayNumber}}</p>
-            <p>每日推荐</p>
+            <br>
+            <p>每日歌曲推荐</p>
         </div>
         <div class="cell content">
             <PlayList :list="dailyList" />
@@ -64,7 +65,7 @@ export default {
         text-align: center;
         color: white;
         font-size: 24px;
-        padding-top: 4em;
+        padding-top: 2em;
     }
     .content {
         flex: 7;
