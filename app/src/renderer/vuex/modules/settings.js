56f69a8ba9d5290da2063195041a4e7e2d020012
rocka <i@rocka.me>
2017-05-02 22:35:24 +0800

    add vuex module settings

---
+++ app/src/renderer/vuex/modules/settings.js
@@ -0,0 +1,24 @@
+import * as types from '../mutation-types';
+import ApiRenderer from '../../util/apirenderer';
+
+let state = {
+    bitRate: 'h',
+    windowBorder: false,
+    autoPlay: false
+};
+
+const mutations = {
+    [types.UPDATE_SETTINGS](state, payload) {
+        state.bitRate = payload.bitRate;
+        state.windowBorder = payload.windowBorder;
+        state.autoPlay = payload.autoPlay;
+    },
+    [types.WRITE_SETTINGS](state) {
+        ApiRenderer.writeSettings(state);
+    }
+};
+
+export default {
+    state,
+    mutations
+};
