3239b3ae6e85079b20eccd9c71cda1ed1982f656
rocka <i@rocka.me>
2017-05-13 22:56:11 +0800

    fix: ApiRenderer gets wrong response sometimes
    ApiRenderer gets same response when request a single api 2 times concurrently with different payload

--- app/src/renderer/util/apirenderer.js
+++ app/src/renderer/util/apirenderer.js
@@ -3,15 +3,18 @@ import { ipcRenderer } from 'electron';
 const methodKeys = ipcRenderer.sendSync('getApiKeys');
 const modules = {};
 
+let invokeId = Date.now();
+
 methodKeys.forEach(methodName => {
     modules[methodName] = function (...args) {
+        invokeId++;
         return new Promise((resolve, reject) => {
-            ipcRenderer.once(methodName, (event, data) => {
+            ipcRenderer.once(`${methodName}${invokeId}`, (event, data) => {
                 if (!PRODUCTION) console.info(methodName, data);
                 if (data.errno) reject(data);
                 else resolve(data);
             });
-            ipcRenderer.send(methodName, ...args);
+            ipcRenderer.send(methodName, invokeId, ...args);
         });
     };
 });
