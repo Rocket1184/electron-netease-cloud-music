277cda1d8ae391b56f430c8b9082f03f8448b6bc
rocka <i@rocka.me>
2017-07-07 21:32:21 +0800

    remove useless console.log

--- src/renderer/util/toast/index.js
+++ src/renderer/util/toast/index.js
@@ -7,7 +7,6 @@ export default {
         const vm = new ToastConstructor().$mount();
         document.body.appendChild(vm.$el);
         $Vue.prototype.$toast = function (msg, timeOut = 1500) {
-            console.log(vm);
             vm.showToast(msg, timeOut);
         };
     }
