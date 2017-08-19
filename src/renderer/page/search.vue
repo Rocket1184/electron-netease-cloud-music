521e8463782729c376993a033e50ea1792044559
rocka <i@rocka.me>
2017-08-19 23:34:44 +0800

    renderer: [search] albums and artists
    can only display results now

--- src/renderer/page/search.vue
+++ src/renderer/page/search.vue
@@ -15,34 +15,105 @@
             <mu-tab value="user"
                 title="用户"></mu-tab>
         </mu-tabs>
-        <div v-if="activeTab === 'song'">
-            <trackList :list="tracks"></trackList>
+        <div v-if="!haveSearched"
+            class="search-tip">
+            <mu-icon value="search"
+                color="grey"
+                :size="128"></mu-icon>
+            <p>右上角搜索框内输入，回车搜索！</p>
+        </div>
+        <div v-else-if="isPosting"
+            class="search-tip">
+            <mu-circular-progress :size="60"
+                :strokeWidth="5"></mu-circular-progress>
+        </div>
+        <div v-else-if="!haveValidResults"
+            class="search-tip">
+            <mu-icon value="inbox"
+                color="grey"
+                :size="128"></mu-icon>
+            <p>哎呀～什么都没找到 ...</p>
+        </div>
+        <div v-else-if="searchError"
+            class="search-tip">
+            <mu-icon value="error_outline"
+                color="grey"
+                :size="128"></mu-icon>
+            <p>哎呀～出错了 ... 错误代码 {{this.searchErrorCode}}</p>
+        </div>
+        <TrackList v-else-if="activeTab === 'song'"
+            :list="tracks"></TrackList>
+        <ArtistList v-else-if="activeTab === 'artist'"
+            :list="artists"></ArtistList>
+        <AlbumList v-else-if="activeTab === 'album'"
+            :list="albums"></AlbumList>
+        <div v-else-if="activeTab === 'playlist'"></div>
+        <div v-else-if="activeTab === 'mv'"></div>
+        <div v-else-if="activeTab === 'user'"></div>
+        <div v-else
+            class="search-tip">
+            <mu-icon value="bug_report"
+                color="grey"
+                :size="128"></mu-icon>
+            <p>你是怎么来到这儿的？来报个 bug 吧少年！</p>
         </div>
     </div>
 </template>
 
 <script>
 import { Track } from '../util/models';
-import trackList from '../components/trackList';
+import TrackList from '../components/trackList';
+import ArtistList from '../components/artistList';
+import AlbumList from '../components/albumList';
 import { searchTypes } from '../util/searchType';
 import ApiRenderer from '../util/apiRenderer';
 
 export default {
     data() {
         return {
+            haveSearched: false,
+            haveValidResults: false,
+            isPosting: false,
+            searchError: false,
+            searchErrorCode: null,
             activeTab: 'song',
-            tracks: []
+            tracks: [],
+            artists: [],
+            albums: []
         };
     },
     methods: {
         handleTabChange(val) {
             this.activeTab = val;
+            this.handleSearch();
         },
         async handleSearch() {
             const { q, t } = this.$route.query;
-            const resp = await ApiRenderer.search(q, t || searchTypes.song);
+            if (!q) return;
+            this.haveSearched = true;
+            this.isPosting = true;
+            const resp = await ApiRenderer.search(q, t || this.activeTab);
+            this.isPosting = false;
             if (resp.code === 200) {
-                this.tracks = resp.result.songs.map(i => new Track(i)) || [];
+                switch (this.activeTab) {
+                    case searchTypes.song:
+                        this.tracks = resp.result.songs.map(i => new Track(i)) || [];
+                        this.haveValidResults = Boolean(this.tracks.length);
+                        break;
+                    case searchTypes.artist:
+                        this.artists = resp.result.artists || [];
+                        this.haveValidResults = Boolean(this.artists.length);
+                        break;
+                    case searchTypes.album:
+                        this.albums = resp.result.albums || [];
+                        this.haveValidResults = Boolean(this.albums.length);
+                        break;
+                    default:
+                        break;
+                }
+            } else {
+                this.searchError = true;
+                this.searchErrorCode = resp.code;
             }
         }
     },
@@ -54,11 +125,20 @@ export default {
         this.handleSearch();
     },
     components: {
-        trackList
+        TrackList,
+        ArtistList,
+        AlbumList
     }
 };
 </script>
 
 <style lang="less">
-
+.search-tip {
+    width: 100%;
+    text-align: center;
+    margin-top: 100px;
+    p {
+        color: grey;
+    }
+}
 </style>
