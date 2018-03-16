5012b8ee38fbed00443c78fa889db651b51e6106
Rocka <i@rocka.me>
2018-03-17 01:50:27 +0800

    main: [preload] fake `electron.shell` `electron.remote.getGlobal`

--- src/main/preload.prod.js
+++ src/main/preload.prod.js
@@ -1,15 +1,19 @@
-const electron = require('electron');
+const { remote, ipcRenderer, shell } = require('electron');
 const platform = require('os').platform();
 
 window.require = function (str) {
     if (str === 'electron') {
-        const electron = require('electron');
+        const versions = process.versions;
         return {
             remote: {
-                getCurrentWindow: electron.remote.getCurrentWindow,
-                getCurrentWebContents: electron.remote.getCurrentWebContents
+                getGlobal: name => name === 'process' ? { versions } : null,
+                getCurrentWindow: remote.getCurrentWindow,
+                getCurrentWebContents: remote.getCurrentWebContents
             },
-            ipcRenderer: electron.ipcRenderer
+            ipcRenderer: ipcRenderer,
+            shell: {
+                openExternal: shell.openExternal
+            }
         };
     } else if (str === 'os') {
         return {
