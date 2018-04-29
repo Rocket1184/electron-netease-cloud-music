22ef75728c3e387f2282a9bfd94c2d60bc553683
Rocka <i@rocka.me>
2018-04-29 18:04:57 +0800

    build: travis deploy to github release

--- script/travis-ci.sh
+++ script/travis-ci.sh
@@ -2,18 +2,29 @@
 
 export NODE_ENV=production
 
+tag() {
+    VER=$(git rev-parse --short HEAD)
+    HASH=$(git log --format=%h -1)
+    git config --local user.name "rocka"
+    git config --local user.email "i@rocka.me"
+    git tag "v$VER-$HASH"
+}
+
 pack() {
-    npm run pack
+    rm dist/.gitkeep
+    yarn run pack
+    npx asar pack dist build/app.asar
 }
 
 build() {
     PLATFORMS=(linux darwin)
     for i in ${PLATFORMS[*]}; do
-        npm run build $i
+        yarn run build "$i"
     done
 }
 
 if pack && build; then
+    tag
     script/upload-build.sh
 else
     exit 1
--- script/upload-build.sh
+++ script/upload-build.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-cd build
+cd build || exit 1
 curl https://dn-devtools.qbox.me/2.1.5/qshell-linux-x64 -o qshell
 chmod +x qshell
-./qshell account $QINIU_AK $QINIU_SK
+./qshell account "$QINIU_AK" "$QINIU_SK"
 
 APP_NAME=electron-netease-cloud-music
 BUCKET_NAME=$APP_NAME
 NCM_RELEASE_CHANNEL=master
-VERSION_HASH=`git rev-parse --short HEAD`
+VERSION_HASH=$(git rev-parse --short HEAD)
 
 ./qshell listbucket $BUCKET_NAME stdout
 
 for ARCH in linux-x64 darwin-x64
 do
-    echo -n $VERSION_HASH > $APP_NAME-$ARCH/ncm_hash
-    TAR_NAME=electron-ncm-$ARCH-$VERSION_HASH.tar.gz
-    tar zcf $TAR_NAME $APP_NAME-$ARCH
-    ./qshell rput $BUCKET_NAME $TAR_NAME $TAR_NAME
+    echo -n "$VERSION_HASH" > "$APP_NAME-$ARCH/ncm_hash"
+    TAR_NAME="electron-ncm-$ARCH-$VERSION_HASH.tar.gz"
+    tar zcf "$TAR_NAME" "$APP_NAME-$ARCH"
+    ./qshell rput "$BUCKET_NAME" "$TAR_NAME" "$TAR_NAME"
 done
