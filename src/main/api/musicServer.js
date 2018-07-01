e3cd77de090bc3302a91debe0069c7176d2e4363
Rocka <i@rocka.me>
2018-07-01 15:48:58 +0800

    main: musicServer: cleanup and refactor
    - replace nested conditional with Guard Clauses
    - write fetched music file stream data to response stream directly
    - check file md5 after downloading finished

--- src/main/api/musicServer.js
+++ src/main/api/musicServer.js
@@ -2,6 +2,8 @@ import fs from 'fs';
 import url from 'url';
 import debug from 'debug';
 import qs from 'querystring';
+import { promisify } from 'util';
+import { createHash } from 'crypto';
 import { createServer } from 'http';
 
 import Cache from './cache';
@@ -9,42 +11,7 @@ import { getMusicUrl } from '.';
 
 const d = debug('MusicServer');
 
-function statAsync(path) {
-    return new Promise((resolve, reject) => {
-        fs.stat(path, (err, stat) => {
-            if (err) reject(err);
-            else resolve(stat);
-        });
-    });
-}
-
-// credit: https://github.com/obastemur/mediaserver/blob/master/index.js#L38-L62
-function getRange(req, total) {
-    // <range-start> - <range-end> / <file-size>
-    const range = [0, total, 0];
-    const rinfo = req.headers ? req.headers.range : null;
-
-    if (rinfo) {
-        const rloc = rinfo.indexOf('bytes=');
-        if (rloc >= 0) {
-            const ranges = rinfo.substr(rloc + 6).split('-');
-            try {
-                range[0] = parseInt(ranges[0]);
-                if (ranges[1] && ranges[1].length) {
-                    range[1] = parseInt(ranges[1]);
-                    range[1] = range[1] < 16 ? 16 : range[1];
-                }
-            } catch (e) {
-                // ignore it
-            }
-        }
-        if (range[1] == total) {
-            range[1]--;
-        }
-        range[2] = total;
-    }
-    return range;
-}
+const statAsync = promisify(fs.stat);
 
 class MusicServer {
     constructor(cacheOrPath) {
@@ -64,79 +31,116 @@ class MusicServer {
         }
     }
 
-    static async serverHandler(cache, req, res) {
-        const location = url.parse(req.url);
-        const params = qs.parse(location.query);
-        d(`Request hit ${location.path}`);
-        if (req.method === 'GET' && location.pathname === '/music') {
-            const id = params['id'];
-            const quality = params['quality'] || 'l';
-            const fileName = `${id}${quality}`;
-            const filePath = cache.fullPath(fileName);
-            if (await cache.has(fileName)) {
-                d(`Hit cache for music id=${id}`);
-                const stat = await statAsync(filePath);
-                const range = getRange(req, stat.size);
-                // TODO: <range-end> may larger than <file-size>, cause file is still being downloaded
-                const file = fs.createReadStream(filePath, { start: range[0], end: range[1] });
-                res.writeHead(206, {
-                    'Cache-Control': 'no-cache',
-                    'Content-Type': 'audio/mpeg',
-                    'Accept-Ranges': 'bytes',
-                    'Content-Range': `bytes ${range[0]}-${range[1]}/${stat.size}`
-                });
-                file.pipe(res);
-            } else {
-                const oUrl = await getMusicUrl(id, quality);
-                if (oUrl.data[0].code === 200) {
-                    d(`Got URL for music id=${id}`);
+    // credit: https://github.com/obastemur/mediaserver/blob/master/index.js#L38-L62
+    static getRange(req, total) {
+        // <range-start> - <range-end> / <file-size>
+        const range = [0, total, 0];
+        const rinfo = req.headers ? req.headers.range : null;
 
-                    const st = await cache.fetch(oUrl.data[0].url, fileName);
-                    st.pipe(fs.createWriteStream(filePath));
-
-                    const realLength = st.headers['content-length'];
-                    const range = getRange(req, realLength);
-                    res.writeHead(206, {
-                        'Cache-Control': 'no-cache',
-                        'Content-Type': 'audio/mpeg',
-                        'Accept-Ranges': 'bytes',
-                        'Content-Range': `bytes ${range[0]}-${range[1]}/${realLength}`
-                    });
-
-                    let stat = await statAsync(filePath);
-                    let file = fs.createReadStream(filePath);
-                    file.pipe(res, { end: false });
-                    // eslint-disable-next-line no-inner-declarations
-                    const endHandler = async function () {
-                        d('stream end but not finished');
-                        const newStat = await statAsync(filePath);
-                        if (newStat.size < realLength) {
-                            if (newStat.size <= stat.size || stat.size == 0) {
-                                d('no more data gain, wait 100ms');
-                                setTimeout(endHandler, 100);
-                            } else {
-                                d(`restart at ${stat.size}`);
-                                file = fs.createReadStream(filePath, { start: stat.size, end: newStat.size - 1 });
-                                file.pipe(res, { end: false });
-                                file.on('end', endHandler);
-                            }
-                            stat = newStat;
-                        } else {
-                            d(`stream ending, total size ${newStat.size}`);
-                            file = null;
-                            fs.createReadStream(filePath, { start: stat.size }).pipe(res);
-                        }
+        if (rinfo) {
+            const rloc = rinfo.indexOf('bytes=');
+            if (rloc >= 0) {
+                const ranges = rinfo.substr(rloc + 6).split('-');
+                try {
+                    range[0] = parseInt(ranges[0]);
+                    if (ranges[1] && ranges[1].length) {
+                        range[1] = parseInt(ranges[1]);
+                        range[1] = range[1] < 16 ? 16 : range[1];
                     }
-                    file.on('end', endHandler);
-                } else {
-                    d(`Cannot get URL for music id=${id}`);
-                    res.writeHead(404);
-                    res.end();
+                } catch (e) {
+                    // ignore it
                 }
             }
-        } else {
-            d('What a Terrible Failure request');
-            res.writeHead(400);
+            if (range[1] == total) {
+                range[1]--;
+            }
+            range[2] = total;
+        }
+        return range;
+    }
+
+    static buildHeaders(range) {
+        return {
+            'Cache-Control': 'no-cache, no-store',
+            'Content-Length': range[1] - range[0] + 1,
+            'Content-Type': 'audio/mpeg',
+            'Accept-Ranges': 'bytes',
+            'Content-Range': `bytes ${range[0]}-${range[1]}/${range[2]}`
+        };
+    }
+
+    /**
+     * MusicServer HTTP request handler
+     * @param {import('http').ClientRequest} req
+     * @param {import('http').ServerResponse} res
+     */
+    async serverHandler(req, res) {
+        const location = url.parse(req.url);
+        const params = qs.parse(location.query);
+        d('Request hit %s', location.path);
+        // check unexpected request method or url
+        if (req.method !== 'GET' || location.pathname !== '/music') {
+            d('What a Terrible Failure');
+            res.writeHead(418, `I'm a teapot`);
+            res.end('@see https://tools.ietf.org/html/rfc2324');
+            return;
+        }
+        const id = params['id'];
+        const quality = params['quality'] || 'l';
+        const fileName = `${id}${quality}`;
+        const filePath = this.cache.fullPath(fileName);
+        // check cache first
+        if (await this.cache.has(fileName)) {
+            d('Hit cache for music id=%d', id);
+            const stat = await statAsync(filePath);
+            const range = MusicServer.getRange(req, stat.size);
+            // TODO: <range-end> may larger than <file-size>, cause file is still being downloaded
+            const file = fs.createReadStream(filePath, { start: range[0], end: range[1] });
+            res.writeHead(206, MusicServer.buildHeaders(range));
+            file.pipe(res);
+            return;
+        }
+        try {
+            const { data: [result] } = await getMusicUrl(id, quality);
+            if (result.code !== 200 || result === null) {
+                throw result.code;
+            }
+            d('Got URL for music id=%d', id);
+
+            const st = await this.cache.fetch(result.url, fileName);
+            st.pipe(fs.createWriteStream(filePath));
+
+            const range = MusicServer.getRange(req, +st.headers['content-length']);
+            res.writeHead(206, MusicServer.buildHeaders(range));
+
+            const checksum = createHash('md5');
+            let receivedByteLength = 0;
+            st.on('data', data => {
+                checksum.update(data);
+                const length = Buffer.byteLength(data);
+                if (receivedByteLength >= range[0]) {
+                    receivedByteLength += length;
+                    res.write(data);
+                    return;
+                }
+                receivedByteLength += length;
+                if (receivedByteLength >= range[0]) {
+                    res.write(data.slice(receivedByteLength - range[0], length));
+                }
+            });
+            st.on('end', () => {
+                const md5 = checksum.digest('hex');
+                if (md5 === result.md5.toLowerCase()) {
+                    d('Finish downloading music id=%d, md5=%s', id, md5);
+                } else {
+                    d('Download music id=%d hash mismatch, delete it ...', id);
+                    this.cache.rm(fileName);
+                }
+                res.end();
+            });
+        } catch (e) {
+            d('Failed to get URL for music id=%d, reason: %O', id, e);
+            res.writeHead(e === 200 ? 400 : 500);
             res.end();
         }
     }
@@ -149,9 +153,9 @@ class MusicServer {
                 this.server.listen(...args);
             }
         } else {
-            this.server = createServer((req, res) => MusicServer.serverHandler(this.cache, req, res));
+            this.server = createServer(this.serverHandler.bind(this));
             this.server.listen(...args);
-            d(`listening on ${JSON.stringify(args)}`);
+            d('listening on %o', args);
         }
     }
 }
