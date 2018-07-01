b88f19cd2778d13af97375c46b86bd1ad88bee6b
Rocka <i@rocka.me>
2018-07-01 16:53:09 +0800

    main: apiHost: remove sync ipc call

--- src/main/apiHost.js
+++ src/main/apiHost.js
@@ -3,10 +3,8 @@ import { ipcMain } from 'electron';
 
 import * as Api from './api';
 
-const d = debug('Api');
-const methodKeys = Reflect.ownKeys(Api).filter(k => typeof Api[k] === 'function');
-
-d('All Api methods:\n%O', methodKeys);
+const TAG = 'Api';
+const d = debug(TAG);
 
 async function rejectTimeout(timeOut = 60000) {
     return await new Promise((_, reject) => {
@@ -16,22 +14,16 @@ async function rejectTimeout(timeOut = 60000) {
     });
 }
 
-methodKeys.map(methodName => {
-    ipcMain.on(methodName, (event, invokeId, ...args) => {
-        d('⬇️ %s', methodName);
-        Promise.race([
-            Api[methodName](...args),
-            rejectTimeout()
-        ]).then(data => {
-            event.sender.send(`${methodName}${invokeId}`, data);
-            d('⬆️ %s', methodName);
-        }).catch(err => {
-            event.sender.send(`${methodName}${invokeId}`, err);
-            d('❌ %s %j', methodName, err);
-        });
+ipcMain.on(TAG, (event, methodName, invokeId, ...args) => {
+    d('↓ %s', methodName);
+    Promise.race([
+        Api[methodName](...args),
+        rejectTimeout()
+    ]).then(data => {
+        event.sender.send(TAG, invokeId, data);
+        d('↑ %s', methodName);
+    }).catch(err => {
+        event.sender.send(TAG, invokeId, err);
+        d('× %s %j', methodName, err);
     });
-});
-
-ipcMain.on('getApiKeys', event => {
-    event.returnValue = methodKeys;
 });
