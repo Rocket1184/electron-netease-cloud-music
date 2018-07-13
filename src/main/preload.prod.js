bbe6fae07eebb85e540c775072ccf11416a73aa9
Rocka <i@rocka.me>
2018-07-14 00:50:55 +0800

    main: preload: node `events`; refactor to switch-case

--- src/main/preload.prod.js
+++ src/main/preload.prod.js
@@ -2,23 +2,29 @@
 
 const { remote, ipcRenderer, shell } = require('electron');
 const { stringify } = require('querystring');
+const { EventEmitter } = require('events');
 const platform = require('os').platform();
 
 window.require = function (id) {
-    if (id === 'electron') {
-        const versions = process.versions;
-        return {
-            remote: {
-                getGlobal: name => name === 'process' ? { versions } : null,
-                getCurrentWindow: remote.getCurrentWindow,
-                getCurrentWebContents: remote.getCurrentWebContents
-            },
-            ipcRenderer: ipcRenderer,
-            shell: { openExternal: shell.openExternal }
-        };
-    } else if (id === 'os') {
-        return { platform: () => platform };
-    } else if (id === 'querystring') {
-        return { stringify };
+    switch (id) {
+        case 'electron':
+            const versions = process.versions;
+            return {
+                remote: {
+                    getGlobal: name => name === 'process' ? { versions } : null,
+                    getCurrentWindow: remote.getCurrentWindow,
+                    getCurrentWebContents: remote.getCurrentWebContents
+                },
+                ipcRenderer: ipcRenderer,
+                shell: { openExternal: shell.openExternal }
+            };
+        case 'os':
+            return { platform: () => platform };
+        case 'querystring':
+            return { stringify };
+        case 'events':
+            return { EventEmitter };
+        default:
+            return {};
     }
 };
