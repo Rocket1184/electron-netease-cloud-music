0a83f6bfc98292e06d28b091fb09f31b898fb932
Rocka <i@rocka.me>
2018-07-29 17:22:35 +0800

    renderer: route: use esm import; add route '/playlist/:id'

--- src/renderer/routes.js
+++ src/renderer/routes.js
@@ -1,30 +1,40 @@
+import Index from '@/page/index.vue';
+import Search from '@/page/search.vue';
+import Player from '@/page/player.vue';
+import Playlist from '@/page/playlist.vue';
+import Settings from '@/page/settings.vue';
+
 export default [
     {
         path: '/',
         title: '个性推荐',
-        component: require('./page/index.vue').default,
+        component: Index,
         icon: 'polymer'
     },
     {
         path: '/search',
         title: '搜索结果',
-        component: require('./page/search.vue').default,
+        component: Search,
         icon: 'search'
     },
     {
         path: '/player',
-        component: require('./page/player.vue').default
+        component: Player
     },
     {
         path: '/playlist',
         title: '我的歌单',
-        component: require('./page/playlist.vue').default,
+        component: Playlist,
         icon: 'library_music'
+    },
+    {
+        path: '/playlist/:id',
+        component: Playlist,
     },
     {
         path: '/settings',
         title: '应用设置',
-        component: require('./page/settings.vue').default,
+        component: Settings,
         icon: 'settings'
     }
 ];
