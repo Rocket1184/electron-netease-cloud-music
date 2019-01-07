06e849218b75e3a5afc19e2d08689b6a1e8f5653
Rocka <i@rocka.me>
2019-01-07 20:35:38 +0800

    renderer: util: fix api type definition

--- src/renderer/util/api/index.d.ts
+++ src/renderer/util/api/index.d.ts
@@ -1,4 +1,4 @@
-import * as ApiFunc from '../../../main/api';
+import * as ApiFunc from '../../../main/api/index';
 
  // TypeScript is MAGIC!
 type Promisfy<T> = T extends ((...args: any[]) => any)
@@ -9,6 +9,6 @@ type Promisfy<T> = T extends ((...args: any[]) => any)
 
 type AllPromisfy<T> = { [P in keyof T]: Promisfy<T[P]> }
 
-declare var Api: AllPromisfy<typeof ApiFunc>;
+declare const Api: AllPromisfy<typeof ApiFunc>;
 
 export default Api;
