fd033e84d9f0471c31ba4e6730c10752af723b9c
Rocka <i@rocka.me>
2019-01-08 13:02:57 +0800

    renderer: FavAlbums: reuse component

--- src/renderer/page/Favorite/FavAlbums.vue
+++ src/renderer/page/Favorite/FavAlbums.vue
@@ -4,21 +4,13 @@
         tipText="登录后查看收藏的专辑"
         :showTip="!user.loginValid">
         <mu-list slot="list">
-            <mu-list-item v-for="al in user.albums"
+            <AvatarListItem v-for="al in user.albums"
                 :key="al.id"
-                avatar
-                button
+                :img="al.picUrl"
+                :title="al.name"
+                :subTitle="`${al.artist.name}, ${al.size} 首`"
                 @click="handleClick(al.id)">
-                <mu-list-item-action>
-                    <mu-avatar>
-                        <img :src="al | picUrl">
-                    </mu-avatar>
-                </mu-list-item-action>
-                <mu-list-item-content>
-                    <mu-list-item-title>{{al.name}}</mu-list-item-title>
-                    <mu-list-item-sub-title>{{al | subtitle}}</mu-list-item-sub-title>
-                </mu-list-item-content>
-            </mu-list-item>
+            </AvatarListItem>
         </mu-list>
         <AlbumDetail slot="detail"
             v-if="ui.fav.album"
@@ -29,8 +21,8 @@
 <script>
 import { mapActions, mapState } from 'vuex';
 
-import { sizeImg, HiDpiPx } from '@/util/image';
 import AlbumDetail from '@/components/AlbumDetail.vue';
+import AvatarListItem from '@/components/AvatarListItem.vue';
 import ListDetailLayout from '@/components/ListDetailLayout.vue';
 
 export default {
@@ -54,14 +46,6 @@ export default {
             this.loadAlbum(id);
         }
     },
-    filters: {
-        picUrl(album) {
-            return sizeImg(album.picUrl, HiDpiPx(40));
-        },
-        subtitle(album) {
-            return `${album.artists.map(ar => ar.name).join(',')}，${album.size} 首`;
-        }
-    },
     mounted() {
         const al = this.user.albums[0];
         if (al && al.id) {
@@ -70,6 +54,7 @@ export default {
     },
     components: {
         AlbumDetail,
+        AvatarListItem,
         ListDetailLayout
     }
 };
