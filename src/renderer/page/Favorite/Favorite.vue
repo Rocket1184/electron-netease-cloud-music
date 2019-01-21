3aa32f7d365440764885475e909c8559bc7b2c57
Rocka <i@rocka.me>
2019-01-21 13:27:19 +0800

    renderer: Favorite: tab slide transition

--- src/renderer/page/Favorite/Favorite.vue
+++ src/renderer/page/Favorite/Favorite.vue
@@ -2,17 +2,20 @@
     <div class="favorite">
         <div v-elevation="4">
             <mu-tabs inverse
-                :value.sync="tab">
-                <mu-tab value="playlist">歌单</mu-tab>
-                <mu-tab value="album">专辑</mu-tab>
-                <mu-tab value="artist">歌手</mu-tab>
-                <mu-tab value="video">视频</mu-tab>
+                :value="tab"
+                @change="handelTabChange">
+                <mu-tab v-for="tab in  favTabs"
+                    :key="tab[0]"
+                    :value="tab[0]">{{tab[1]}}</mu-tab>
             </mu-tabs>
         </div>
         <div class="fav-content">
-            <keep-alive>
-                <component :is="favCompo"></component>
-            </keep-alive>
+            <transition :name="transitionName"
+                mode="out-in">
+                <keep-alive>
+                    <component :is="favCompo"></component>
+                </keep-alive>
+            </transition>
         </div>
     </div>
 </template>
@@ -22,6 +25,13 @@ import FavAlbums from './FavAlbums.vue';
 import FavVideos from './FavVideos.vue';
 import FavArtists from './FavArtists.vue';
 import FavPlaylists from './FavPlaylists.vue';
+
+const FavTabs = [
+    ['playlist', '歌单'],
+    ['album', '专辑'],
+    ['artist', '歌手'],
+    ['video', '视频'],
+];
 
 const FavCompos = {
     album: FavAlbums,
@@ -34,13 +44,30 @@ export default {
     name: 'page-favorite',
     data() {
         return {
-            tab: 'playlist'
+            tab: 'playlist',
+            favTabs: FavTabs,
+            transitionName: 'slide-left'
         };
     },
     computed: {
         favCompo() {
             return FavCompos[this.tab];
         }
+    },
+    methods: {
+        handelTabChange(val) {
+            let oldIndex, newIndex;
+            FavTabs.forEach((tab, index) => {
+                if (tab[0] === this.tab) oldIndex = index;
+                if (tab[0] === val) newIndex = index;
+            });
+            if (newIndex < oldIndex) {
+                this.transitionName = 'slide-right';
+            } else {
+                this.transitionName = 'slide-left';
+            }
+            this.tab = val;
+        },
     },
     components: {
         FavAlbums,
@@ -58,6 +85,7 @@ export default {
     flex-direction: column;
     .fav-content {
         height: calc(~'100% - 48px');
+        overflow-x: hidden;
     }
 }
 </style>
