1a0e3e18db36f2e4f673f4a1376878029368ac7d
Rocka <i@rocka.me>
2019-01-25 20:49:42 +0800

    renderer: vuex: remove setting option `autoPlay`

--- src/renderer/vuex/modules/settings.js
+++ src/renderer/vuex/modules/settings.js
@@ -3,7 +3,6 @@ import * as types from '../mutation-types';
 let state = window.__NCM_SETTINGS__ || {
     bitRate: 'l',
     windowBorder: true,
-    autoPlay: false,
     themePrimaryColor: '#7e57c2',
     themeSecondaryColor: '#ff4081',
     themeVariety: 'light'
