fdce68723f68458273f6b938b20a4959f7be8cbd
Rocka <i@rocka.me>
2019-01-31 17:17:17 +0800

    renderer: vuex: mutation for remove track from playlist

--- src/renderer/vuex/modules/playlist.js
+++ src/renderer/vuex/modules/playlist.js
@@ -36,18 +36,31 @@ const mutations = {
     [types.SET_LOOP_MODE_RANDOM](state) {
         state.loopMode = LOOP_MODE.RANDOM;
     },
-    [types.RESTORE_PLAYLIST](state, payload) {
-        const { index, loopMode, list } = payload;
+    [types.RESTORE_PLAYLIST](state, { index, loopMode, list }) {
         state.index = index || 0;
         state.loopMode = loopMode || LOOP_MODE.LIST;
         state.list = list.map(t => new Track(t));
     },
-    [types.INSERT_TRACK_INTO_PLAYLIST](state, payload) {
-        const { tracks, index } = payload;
+    [types.INSERT_TRACK_INTO_PLAYLIST](state, { tracks, index }) {
         state.list.splice(index, 0, ...tracks);
         if (index <= state.index) {
             // keep current track unchanged
             state.index += tracks.length;
+        }
+    },
+    [types.REMOVE_TRACK_FROM_PLAYLIST](state, { start, count }) {
+        state.list.splice(start, count);
+        const end = start + count - 1;
+        if (start > state.index) return;
+        if (end < state.index) {
+            state.index -= count;
+            return;
+        }
+        // start <= index && end >= index
+        if (start < state.list.length) {
+            state.index = start;
+        } else {
+            state.index = state.list.length - 1;
         }
     }
 };
