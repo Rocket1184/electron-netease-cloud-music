64bc97a6b067864720b3bc31250fbd862f0680cd
Rocka <i@rocka.me>
2019-02-06 12:33:38 +0800

    renderer: api: some rename

--- src/renderer/util/api/index.js
+++ src/renderer/util/api/index.js
@@ -6,26 +6,26 @@ import { ipcRenderer } from 'electron';
 const TAG = 'API';
 const d = debug(TAG);
 
-let invokeId = 0;
+let resolveId = 0;
 
 const methodMap = new Map();
-const actionMap = new Map();
+const resolveMap = new Map();
 
 ipcRenderer.on(TAG, (_, id, data) => {
-    if (actionMap.has(id)) {
+    if (resolveMap.has(id)) {
         d('🔻 %d %o', id, data);
-        const action = actionMap.get(id);
-        action(data);
-        actionMap.delete(id);
+        const resolve = resolveMap.get(id);
+        resolve(data);
+        resolveMap.delete(id);
     }
 });
 
 function senderFn(methodName, ...args) {
-    invokeId++;
+    resolveId++;
     return new Promise(resolve => {
-        actionMap.set(invokeId, resolve);
-        ipcRenderer.send(TAG, methodName, invokeId, ...args);
-        d('🔺 %d %s %o', invokeId, methodName, args);
+        resolveMap.set(resolveId, resolve);
+        ipcRenderer.send(TAG, methodName, resolveId, ...args);
+        d('🔺 %d %s %o', resolveId, methodName, args);
     });
 }
 
@@ -34,7 +34,7 @@ export default new Proxy({}, {
         if (methodMap.has(propName)) {
             return methodMap.get(propName);
         }
-        const fn = senderFn.bind(this, propName);
+        const fn = senderFn.bind(null, propName);
         methodMap.set(propName, fn);
         return fn;
     }
