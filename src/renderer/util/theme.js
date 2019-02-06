9800fae4f099fd2f9499291f275615a45cd7e40e
Rocka <i@rocka.me>
2019-02-06 17:09:06 +0800

    renderer: util: add comment to theme configuration functions

--- src/renderer/util/theme.js
+++ src/renderer/util/theme.js
@@ -4,16 +4,26 @@ const overrides = {
     divider: 'rgba(0,0,0,.2)'
 };
 
-export function initTheme(...args) {
+/**
+ * add app specific theme variable, then set theme color
+ * @param {any} theme theme option
+ * @param {string} extendName theme name to extend
+ */
+export function initTheme(theme, extendName) {
     MuseUI.theme.addCreateTheme((theme) => {
         return `
 .player-bar,.ncm-page{background-color:${theme.background.default}}
 .action-item:hover .mu-icon{color:${theme.background.default}}
 `.trim();
     });
-    setTheme(...args);
+    setTheme(theme, extendName);
 }
 
+/**
+ * set MuseUI theme color
+ * @param {any} theme theme option
+ * @param {string} extendName theme name to extend
+ */
 export function setTheme(theme, extendName) {
     const id = 'ncm';
     MuseUI.theme.add(id, { ...overrides, ...theme }, extendName).use(id);
