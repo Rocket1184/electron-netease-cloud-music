a72e6288aa68f03eb2e867539516e837e33db7b8
Rocka <i@rocka.me>
2019-02-27 00:52:22 +0800

    renderer: vuex: do not insert duplicate track to radio

--- src/renderer/vuex/modules/radio.js
+++ src/renderer/vuex/modules/radio.js
@@ -7,15 +7,30 @@ const state = {
     loopMode: LOOP_MODE.LIST
 };
 
+const trackIdMap = new Map();
+
 const mutations = {
     [types.RESTORE_RADIO](state, { list, index }) {
-        state.list = list;
         state.index = index;
+        list.forEach(t => {
+            if (!trackIdMap.has(t.id)) {
+                trackIdMap.set(t.id, true);
+                state.list.push(t);
+            }
+        });
     },
     [types.APPEND_RADIO](state, { tracks }) {
-        state.list.push.apply(state.list, tracks);
+        const toAppend = [];
+        tracks.forEach(t => {
+            if (!trackIdMap.has(t.id)) {
+                trackIdMap.set(t.id, true);
+                toAppend.push(t);
+            }
+        });
+        state.list.push.apply(state.list, toAppend);
         if (state.list.length > 150) {
             state.list.splice(0, state.length - 150);
+            state.index -= toAppend.length;
         }
     },
     [types.REMOVE_RADIO](state, { id }) {
@@ -26,6 +41,7 @@ const mutations = {
                 state.index--;
             }
             state.list.splice(i, 1);
+            trackIdMap.delete(id);
         }
     },
     [types.SET_RADIO_INDEX](state, payload) {
