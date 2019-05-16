958f7b919ca45239b4048428ff2b16531be6f0a3
Rocka <i@rocka.me>
2019-05-16 23:50:14 +0800

    renderer: add compo PlayTracksButton alternative  to PlayTracks

---
+++ src/renderer/components/PlayTracksButton.vue
@@ -0,0 +1,23 @@
+<template>
+    <mu-button flat
+        v-bind="$attrs"
+        @click="handlePlayAll">
+        <mu-icon left
+            value="play_circle_outline"></mu-icon>
+        <span>{{btnPlayText}}</span>
+    </mu-button>
+</template>
+
+<script>
+import PlayTracks from '@/components/PlayTracks.vue';
+
+export default {
+    extends: PlayTracks,
+    inheritAttrs: false,
+    computed: {
+        btnPlayText() {
+            return `播放全部 (${this.tracks.length})`;
+        }
+    }
+};
+</script>
