8df905ef90c90aab25acf796e19052ba3ea34c67
Rocka <i@rocka.me>
2019-09-07 22:33:09 +0800

    main: fake `bindings` as commonjs module

--- src/main/mpris/fake-bindings.js
+++ src/main/mpris/fake-bindings.js
@@ -1,8 +1,8 @@
 // @ts-nocheck
 /* eslint-disable */
 
-import path from 'path';
+const path = require('path');
 
-export default function bindings() {
-    return __non_webpack_require__.resolve(path.join(__dirname, 'build/bindings.node'));
+module.exports = function bindings() {
+    return __non_webpack_require__(path.join(__dirname, 'build/bindings.node'));
 }
