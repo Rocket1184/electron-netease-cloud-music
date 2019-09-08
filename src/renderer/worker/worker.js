ea859feb02eec37545e03e8bf285ae1b2b3b8568
Rocka <i@rocka.me>
2019-09-08 21:28:24 +0800

    renderer: worker: image approximate brightness calculation function

--- src/renderer/worker/worker.js
+++ src/renderer/worker/worker.js
@@ -23,10 +23,36 @@ export function filterTracks(w, tracks) {
 
 MethodMap.set('filterTracks', filterTracks);
 
-self.onmessage = function (ev) {
+const oscvs = new OffscreenCanvas(64, 64);
+const oscvsCtx = oscvs.getContext('2d');
+
+/**
+ * @param {ImageBitmapSource} bms
+ * @returns {Promise<number>}
+ * @see https://stackoverflow.com/a/596243/8370777
+ */
+export async function determineBrightness(bms) {
+    const bm = await createImageBitmap(bms);
+    oscvsCtx.drawImage(bm, 0, 0, 64, 64);
+    const dt = oscvsCtx.getImageData(0, 0, 64, 64);
+    let brightness = 0;
+    let rgb = [0, 0, 0];
+    for (let i = 0; i < dt.data.length; i += 4) {
+        rgb[0] += dt.data[i];
+        rgb[1] += dt.data[i + 1];
+        rgb[2] += dt.data[i + 2];
+    }
+    brightness = (0.299 * rgb[0] + 0.587 * rgb[1] + 0.114 * rgb[2]) / 4096;
+    oscvsCtx.clearRect(0, 0, 64, 64);
+    return brightness;
+}
+
+MethodMap.set('determineBrightness', determineBrightness);
+
+self.onmessage = async (ev) => {
     const method = MethodMap.get(ev.data.method);
     if (method) {
-        const result = method.apply(null, ev.data.args);
+        const result = await method.apply(null, ev.data.args);
         const msg = { id: ev.data.id, method: ev.data.method, result };
         // @ts-ignore
         self.postMessage(msg);
