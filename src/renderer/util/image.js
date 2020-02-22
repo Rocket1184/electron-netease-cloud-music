5b7ac5c61ac0093abe050131dd241590fc8de8bc
Rocka <i@rocka.me>
2020-02-22 22:55:04 +0800

    renderer: util: `sizeImg` to integer size; jsdoc image functions
    if sizeImg receives float somehow, the image size would collapse

--- src/renderer/util/image.js
+++ src/renderer/util/image.js
@@ -1,18 +1,34 @@
+/**
+ * @param {string} url
+ * @param {number} width
+ * @param {number} height
+ */
 export function sizeImg(url, width, height = width) {
-    if (url === null || url === undefined) url = '';
+    if (!url) return '';
     if (url.startsWith('http:')) url = 'https' + url.slice(4);
-    return `${url}?param=${width}y${height}`;
+    return `${url}?param=${width.toFixed(0)}y${height.toFixed(0)}`;
 }
 
+/**
+ * @param {string} url
+ */
 export function bkgImg(url) {
-    if (url === null || url === undefined) url = '';
+    if (!url) return '';
     return `background-image:url(${url})`;
 }
 
+/**
+ * @param {number|string} id
+ * @param {number} width
+ * @param {number} height
+ */
 export function blurImg(id, width, height = width) {
     return `https://music.163.com/api/img/blur/${id}?param=${width}y${height}`;
 }
 
+/**
+ * @param {number} px
+ */
 export function HiDpiPx(px) {
     return px * window.devicePixelRatio;
 }
